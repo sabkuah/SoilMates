4ef0d615a0734774b09f7e8eb6875cafd9823bcf
Sabrina Kuah <sabrina@Sabrinas-MacBook-Pro.local>
2021-02-01 14:46:14 -0800

    Register routes added, #17

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+const passport = require("passport");
+
+//========================
+//       REGISTER
+//========================
+
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log("New User Registered >>>", registeredUser);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Soil-Mates!");
+        res.redirect("/plants");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 //========================
 //        LOGIN
@@ -10,13 +40,19 @@ router.get("/login", (req, res) => {
   res.render("users/login");
 });
 
-//========================
-//       REGISTER
-//========================
-
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back to Soil-Mates");
+    const redirectUrl = req.session.returnUrl || "/plants";
+    delete req.session.returnUrl;
+    res.redirect(redirectUrl);
+  }
+);
 
 //========================
 //         LOGOUT
